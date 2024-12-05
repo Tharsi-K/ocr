7fc9f41b7b0fa3ce1fd5d6c5e7599a721b39509d
Krishnamoorthy Ilakkanan <121012672+Ilakkanan@users.noreply.github.com>
2024-12-05 11:20:57 +0530

    home page

--- ocr-frontend/src/App.jsx
+++ ocr-frontend/src/App.jsx
@@ -20,30 +20,32 @@ import ManageBooks from './pages/Admin/ManageBooks';
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/book/:bookId' element={<Book />} />
-        <Route path='/search' element={<Search />} />
-        <Route element={<PrivateRoute allowedRoles={['User', 'Admin']} />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/add-book' element={<AddBook />} />
-          <Route path='/update-book/:bookId' element={<UpdateBook />} />
-        </Route>
-        {/* Admin-only route */}
-        <Route element={<PrivateRoute allowedRoles={['Admin']} />}>
-          <Route path="admin/*" element={<AdminPanel />}>
-            <Route index element={<Dashboard />} /> {/* Default to Dashboard */}
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="manage-users" element={<ManageUsers />} />
-            <Route path="manage-books" element={<ManageBooks />} />
+      <div className="min-h-screen bg-white">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/book/:bookId' element={<Book />} />
+          <Route path='/search' element={<Search />} />
+          <Route element={<PrivateRoute allowedRoles={['User', 'Admin']} />}>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/add-book' element={<AddBook />} />
+            <Route path='/update-book/:bookId' element={<UpdateBook />} />
           </Route>
-        </Route>
-        <Route path='/ocr' element={<OCR />} />
-      </Routes>
+          {/* Admin-only route */}
+          <Route element={<PrivateRoute allowedRoles={['Admin']} />}>
+            <Route path="admin/*" element={<AdminPanel />}>
+              <Route index element={<Dashboard />} /> {/* Default to Dashboard */}
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="manage-users" element={<ManageUsers />} />
+              <Route path="manage-books" element={<ManageBooks />} />
+            </Route>
+          </Route>
+          <Route path='/ocr' element={<OCR />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
